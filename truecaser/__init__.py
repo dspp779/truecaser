8c79d2b92683c9d8d827caa752a6e8277cb4543b
JJC <jjc@nlplab.cc>
2019-01-22 11:00:46 +0800

    package __init__

---
+++ truecaser/__init__.py
@@ -0,0 +1,6 @@
+from abstract_truecaser import AbstractTruecaser
+from greedy_truecaser import GreedyTruecaser
+from neural_truecaser import NeuralTruecaser
+from statistical_truecaser import StatisticalTruecaser
+
+__all__ = ['AbstractTruecaser', 'GreedyTruecaser', 'NeuralTruecaser', 'StatisticalTruecaser']
